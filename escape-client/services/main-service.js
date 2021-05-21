0243a5c6c1e9e05fa814450eb6517bbe92dc4b03
Whitney Smith <whitney.smith.lee@gmail.com>
2021-05-21 15:40:25 -0700

    test

--- escape-client/services/main-service.js
+++ escape-client/services/main-service.js
@@ -2,11 +2,9 @@ const BASE_URL = 'https://escape-travel-app.herokuapp.com/';
 // const TEST_URL = 'http://localhost:3001/';
 
 function fetchMain() {
-    return fetch(`${BASE_URL}`, {
+    return fetch(`/`, {
     method: 'GET',
     headers: new Headers({'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://escape-app-server.herokuapp.com/',}),
-    mode: 'no-cors',
     })
     .then(res => {
       if(res.ok) return res.json();
